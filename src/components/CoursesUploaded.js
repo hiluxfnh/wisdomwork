4b8c85059339ca9f0863fb189d4ba05fd5cd6c8c
Hilux Fokou Ngoumo <103578226+hiluxfnh@users.noreply.github.com>
2024-08-24 23:52:56 +0530

    new changes

--- src/components/CoursesUploaded.js
+++ src/components/CoursesUploaded.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import '../styles/CoursesUploaded.css';
+import picture from '../images/picture.png';
 
 const CoursesUploaded = () => {
     return (
         <div className="courses-uploaded">
             <h2>Courses Uploaded Recently</h2>
             <div className="course-card">
-                <img src="course-image.png" alt="Graphic Design" />
+                <img src={picture} alt="Graphic Design" />
                 <div className="course-info">
-                    <h3>Graphic Design</h3>
-                    <p>Where professionals create visual content to communicate messages.</p>
-                    <p>265.75k students enrolled</p>
-                    <button className="edit-btn">EDIT</button>
+                    <div className="course-tags">
+                        <h5>GRAPHIC DESIGN</h5> <h5>UI/UX DESIGN</h5>
+                    </div>
+                    <div className="course-head">
+                        <div className="course-description">
+                            <p><span>Graphics Design: </span>Where professionals create visual content to communicate messages.</p>
+                        </div>
+                        <div className="course-data">
+                            <button className="edit-btn">EDIT</button>
+                            <p>265.75k students enrolled</p>
+                        </div> 
+                    </div>              
                 </div>
             </div>
         </div>
